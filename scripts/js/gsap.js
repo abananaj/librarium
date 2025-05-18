6f86d013ca3c325e62c9497636888adf8af65d95
abananaj <anna@theatrum.dev>
2025-05-18 16:51:34 -0700

    cheatsheet finally fucking working

--- scripts/js/gsap.js
+++ scripts/js/gsap.js
@@ -35,17 +35,3 @@ import { SplitText } from "gsap/SplitText";
 
 gsap.registerPlugin(useGSAP, Flip, ScrollTrigger, Observer, ScrollToPlugin, Draggable, MotionPathPlugin, EaselPlugin, PixiPlugin, TextPlugin, DrawSVGPlugin, ScrollSmoother, GSDevTools, InertiaPlugin, MorphSVGPlugin, MotionPathHelper, Physics2DPlugin, PhysicsPropsPlugin, ScrambleTextPlugin, SplitText, RoughEase, ExpoScaleEase, SlowMo, CustomEase, CustomBounce, CustomWiggle);
 // Import just what we need
-
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-// import "./js/color-modes.js";
-// import 'bootstrap/js/dist/button';
-// import 'bootstrap/js/dist/carousel';
-// import 'bootstrap/js/dist/collapse';
-// import 'bootstrap/js/dist/dropdown';
-// import 'bootstrap/js/dist/modal';
-// import 'bootstrap/js/dist/offcanvas';
-// import 'bootstrap/js/dist/popover';
-// import 'bootstrap/js/dist/scrollspy';
-// import 'bootstrap/js/dist/tab';
-// import 'bootstrap/js/dist/toast';
-// import 'bootstrap/js/dist/tooltip';
--- scripts/js/index.js
+++ scripts/js/index.js
@@ -1,50 +1,5 @@
-// register all plugins
+import "./gsap.js";
 
-import { gsap } from "gsap";
-import { useGSAP } from "@gsap/react";
+import "bootstrap/dist/js/bootstrap.js";
+import "bootstrap/dist/js/bootstrap.bundle.js";
 
-import { CustomEase } from "gsap/CustomEase";
-import { RoughEase, ExpoScaleEase, SlowMo } from "gsap/EasePack";
-
-/* The following eases are Club GSAP perks */
-import { CustomBounce } from "gsap/CustomBounce"; // extends CustomEase
-import { CustomWiggle } from "gsap/CustomWiggle"; // extends CustomEase
-
-import { Flip } from "gsap/Flip";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { Observer } from "gsap/Observer";
-import { ScrollToPlugin } from "gsap/ScrollToPlugin";
-import { Draggable } from "gsap/Draggable";
-import { MotionPathPlugin } from "gsap/MotionPathPlugin";
-import { EaselPlugin } from "gsap/EaselPlugin";
-import { PixiPlugin } from "gsap/PixiPlugin";
-import { TextPlugin } from "gsap/TextPlugin";
-
-/* The following plugins are Club GSAP perks */
-import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
-import { ScrollSmoother } from "gsap/ScrollSmoother";
-import { GSDevTools } from "gsap/GSDevTools";
-import { InertiaPlugin } from "gsap/InertiaPlugin";
-import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
-import { MotionPathHelper } from "gsap/MotionPathHelper";
-import { Physics2DPlugin } from "gsap/Physics2DPlugin";
-import { PhysicsPropsPlugin } from "gsap/PhysicsPropsPlugin";
-import { ScrambleTextPlugin } from "gsap/ScrambleTextPlugin";
-import { SplitText } from "gsap/SplitText";
-
-
-gsap.registerPlugin(useGSAP, Flip, ScrollTrigger, Observer, ScrollToPlugin, Draggable, MotionPathPlugin, EaselPlugin, PixiPlugin, TextPlugin, DrawSVGPlugin, ScrollSmoother, GSDevTools, InertiaPlugin, MorphSVGPlugin, MotionPathHelper, Physics2DPlugin, PhysicsPropsPlugin, ScrambleTextPlugin, SplitText, RoughEase, ExpoScaleEase, SlowMo, CustomEase, CustomBounce, CustomWiggle);
-// Import just what we need
-
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-// import 'bootstrap/js/dist/button';
-// import 'bootstrap/js/dist/carousel';
-// import 'bootstrap/js/dist/collapse';
-// import 'bootstrap/js/dist/dropdown';
-// import 'bootstrap/js/dist/modal';
-// import 'bootstrap/js/dist/offcanvas';
-// import 'bootstrap/js/dist/popover';
-// import 'bootstrap/js/dist/scrollspy';
-// import 'bootstrap/js/dist/tab';
-// import 'bootstrap/js/dist/toast';
-// import 'bootstrap/js/dist/tooltip';
