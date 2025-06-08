878369945acea762bf96e73939ecb6f4f0b0e9f9
abananaj <anna@theatrum.dev>
2025-06-07 22:03:33 -0700

    add webpack

---
+++ _webpack/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+module.exports = {
+    // mode: 'development',
+    devtool: 'eval-source-map',
+    entry: './development/index.ts',   
+    output: {
+        publicPath: 'production',
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'production'),
+    },
+    devServer: {
+        static: path.resolve(__dirname, 'production'),
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    module: {
+        rules:
+            [{
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: 'ts-loader',
+            },
+                // {
+                //     test: /\.scss$/,
+                //     exclude: /node_modules/,
+                //     use: ['style-loader', 'css-loader', 'sass-loader']
+                // },
+                // {
+                //     test: /\.js$/,
+                //     exclude: /node_modules/,
+                //     use: {
+                //         loader: 'babel-loader',
+                //         options: {
+                //             presets: ['@babel/preset-env']
+                //         }
+                //     }
+                // }
+            ]
+    }
+};
