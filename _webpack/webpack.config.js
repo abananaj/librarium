9e4fd1dacb17bc32d54d74baa4c6aa9e3a7e2fff
abananaj <anna@theatrum.dev>
2025-06-07 22:51:47 -0700

    ignore webpack

--- _webpack/webpack.config.js
+++ _webpack/webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
+const Webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
 
 module.exports = {
-    // mode: 'development',
+    mode: 'development',
     devtool: 'eval-source-map',
-    entry: './development/index.ts',   
+    entry: './development/index.ts',
     output: {
         publicPath: 'production',
         filename: 'bundle.js',
@@ -22,11 +24,15 @@ module.exports = {
                 exclude: /node_modules/,
                 use: 'ts-loader',
             },
-                // {
-                //     test: /\.scss$/,
-                //     exclude: /node_modules/,
-                //     use: ['style-loader', 'css-loader', 'sass-loader']
-                // },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
                 // {
                 //     test: /\.js$/,
                 //     exclude: /node_modules/,
